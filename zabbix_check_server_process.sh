f4238e5a3c819bfa9ef2ad30e8dbc8627fbbb22c
guodonghe <1003993630@qq.com>
2025-01-13 16:36:37 +0800

    Update zabbix_check_server_process.sh

--- zabbix_check_server_process.sh
+++ zabbix_check_server_process.sh
@@ -34,7 +34,12 @@ _SERVER_STATUS(){
     /usr/bin/supervisorctl status ${1} | /bin/awk '{print $2}' |/bin/grep -q "RUNNING" 2>/dev/null 
     echo $?
 }
-
+#启动市场
+_SERVER_UPTIME(){
+    pid=`/usr/bin/supervisorctl status ${1} |/bin/awk -F ',' '{print $1}'|/bin/awk '{print $4}'`
+    uptime=`ps -p $pid -o etime|/bin/grep -v ELAPSED|/bin/awk -F ':' '{print $(NF-1)}'`
+    echo $uptime
+}
 
 
 #执行过程
@@ -45,6 +50,20 @@ fi
 if [ "x$1" == "xdiscovery" ];then
     _SERVER_PROCESS
 fi
+#
+if [ "x$1" == "xuptime" ];then
+    _SERVER_UPTIME "$2"
+fi
 
-# 实例 
+# 实例一
 sh zabbix_check_server_process.sh echo pic_excel
+# 实例二
+/bin/bash zabbix_check_server_process.sh discovery  
+
+
+# 触发器一
+{Ali Template Supervisor:server_process.discovery[echo,{#SERVERPROCESS}].avg(#3)}>0.5 and {Ali Template Supervisor:server_process.discovery[echo,{#SERVERPROCESS}].last()}<>0
+#触发器二
+{Ali Template Supervisor:server_process.discovery[uptime,{#SERVERPROCESS}].max(#3)}<5
+
+
